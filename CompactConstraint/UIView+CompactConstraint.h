7b61ff93aa3d7b910913e01de62f644369a810fb
Marco Arment <arment@marco.org>
2021-12-06 09:06:47 -0500

    Fixed incorrect nullability annotations

--- CompactConstraint/UIView+CompactConstraint.h
+++ CompactConstraint/UIView+CompactConstraint.h
@@ -14,10 +14,10 @@
 @interface CCView (CompactConstraint)
 
 // Add a single constraint with the compact syntax
-- (NSLayoutConstraint * _Nullable)addCompactConstraint:(NSString * _Nonnull)relationship metrics:(NSDictionary * _Nullable)metrics views:(NSDictionary * _Nullable)views;
+- (NSLayoutConstraint * _Nonnull)addCompactConstraint:(NSString * _Nonnull)relationship metrics:(NSDictionary * _Nullable)metrics views:(NSDictionary * _Nullable)views;
 
 // Add any number of constraints. Can also mix in Visual Format Language strings.
-- (NSArray * _Nullable)addCompactConstraints:(NSArray * _Nonnull)relationshipStrings metrics:(NSDictionary * _Nullable)metrics views:(NSDictionary * _Nullable)views;
+- (NSArray<NSLayoutConstraint *> * _Nonnull)addCompactConstraints:(NSArray * _Nonnull)relationshipStrings metrics:(NSDictionary * _Nullable)metrics views:(NSDictionary * _Nullable)views;
 
 // And a convenient shortcut for what we always end up doing with the visualFormat call.
 - (void)addConstraintsWithVisualFormat:(NSString * _Nonnull)format options:(NSLayoutFormatOptions)opts metrics:(NSDictionary * _Nullable)metrics views:(NSDictionary * _Nullable)views;
